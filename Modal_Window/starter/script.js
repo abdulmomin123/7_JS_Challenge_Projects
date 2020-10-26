7cab7397535fda4977d8c2c2540a10531ffe2266
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-26 21:09:34 +0600

    all

--- Modal_Window/starter/script.js
+++ Modal_Window/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const buttons = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+buttons.forEach(button => button.addEventListener('click', openModal));
+overlay.addEventListener('click', closeModal);
+
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+function closeModal() {
+  modal.classList.add('hidden');
+  this.classList.add('hidden');
+}
