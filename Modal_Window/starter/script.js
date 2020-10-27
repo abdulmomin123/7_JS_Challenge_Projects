70a84654d06c69ee34b7bc61332a154e8813253c
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-27 18:33:06 +0600

    all

--- Modal_Window/starter/script.js
+++ Modal_Window/starter/script.js
@@ -1,16 +1,30 @@
 'use strict';
 
-const buttons = document.querySelectorAll('.show-modal');
-const openingElements = document.querySelectorAll('.modal, .overlay');
-const closingElements = document.querySelectorAll('.close-modal, .overlay');
+// const buttons = document.querySelectorAll('.show-modal');
+// const openingElements = document.querySelectorAll('.modal, .overlay');
+// const closingElements = document.querySelectorAll('.close-modal, .overlay');
 
-const openModal = () => {
-  openingElements.forEach(el => el.classList.remove('hidden'));
-};
+// const openModal = () => {
+//   openingElements.forEach(el => el.classList.remove('hidden'));
+// };
 
-const closeModal = () => {
-  openingElements.forEach(el => el.classList.add('hidden'));
-};
+// const closeModal = () => {
+//   openingElements.forEach(el => el.classList.add('hidden'));
+// };
 
-buttons.forEach(btn => btn.addEventListener('click', openModal));
-closingElements.forEach(el => el.addEventListener('click', closeModal));
+// buttons.forEach(btn => btn.addEventListener('click', openModal));
+// closingElements.forEach(el => el.addEventListener('click', closeModal));
+
+function foo() {
+  const a = 2;
+  const b = 3;
+  const c = 3;
+
+  return function () {
+    console.log(a);
+  };
+}
+
+const bar = foo();
+
+console.dir(bar);
