b996cb0747f0297184e1199425c75c961bde7b0a
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-28 07:01:35 +0600

    all

--- Guess_My_Number/starter/script.js
+++ Guess_My_Number/starter/script.js
@@ -1,11 +1,1 @@
 'use strict';
-
-console.log((2 ** 53 - 1).toString().length);
-console.log(Number.MAX_SAFE_INTEGER);
-console.log(11111111111111111111111111111111111111111);
-
-// Big int!
-console.log(
-  11111111111111111111111111111111111111111n +
-    11111111111111111111111111111111111111111n
-);
