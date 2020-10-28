12bc1700ea9207c4c6b61e3666af16de5f79c911
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-28 07:38:36 +0600

    all

--- Guess_My_Number/starter/script.js
+++ Guess_My_Number/starter/script.js
@@ -9,3 +9,30 @@ const elements = {
   number: document.querySelector('.number'),
   again: document.querySelector('.again'),
 };
+
+let secretNum = Math.floor(Math.random() * 20 + 1);
+
+function test() {
+  console.log('hi');
+}
+
+function evalGuess() {
+  //
+}
+
+function win() {
+  //
+}
+
+function lost() {
+  //
+}
+
+function again() {
+  //
+}
+
+elements.check.addEventListener('click', evalGuess);
+elements.again.addEventListener('click', again);
+
+console.log(secretNum);
