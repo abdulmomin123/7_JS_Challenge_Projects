356c9258342a7f1471600a2b388939c93d31eb8e
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-28 07:28:00 +0600

    all

--- Guess_My_Number/starter/script.js
+++ Guess_My_Number/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const elements = {
+  userInput: document.querySelector('.guess'),
+  check: document.querySelector('.check'),
+  hint: document.querySelector('.message'),
+  score: document.querySelector('.label-score'),
+  highScore: document.querySelector('.label-highscore'),
+  number: document.querySelector('.number'),
+  again: document.querySelector('.again'),
+};
