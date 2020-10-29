89a3e487f69b93cf8c4f5b463017fa5c94a3040a
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-29 07:04:58 +0600

    all

--- Pig_Game/starter/script.js
+++ Pig_Game/starter/script.js
@@ -7,6 +7,10 @@ const elements = {
   newGame: document.querySelector('.btn--new'),
 
   //   UI elements
+  score0: document.getElementById('score--0'),
+  current0: document.getElementById('current--0'),
+  score1: document.getElementById('score--1'),
+  current1: document.getElementById('current--1'),
 };
 
 function rollDice() {
