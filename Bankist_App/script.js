a040c8e32b053ed1f4ae934aefe3137f4887d147
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-31 17:06:03 +0600

    all

--- Bankist_App/script.js
+++ Bankist_App/script.js
@@ -32,30 +32,32 @@ const account4 = {
 const accounts = [account1, account2, account3, account4];
 
 // Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
+const elements = {
+  labelWelcome: document.querySelector('.welcome'),
+  labelDate: document.querySelector('.date'),
+  labelBalance: document.querySelector('.balance__value'),
+  labelSumIn: document.querySelector('.summary__value--in'),
+  labelSumOut: document.querySelector('.summary__value--out'),
+  labelSumInterest: document.querySelector('.summary__value--interest'),
+  labelTimer: document.querySelector('.timer'),
 
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
+  containerApp: document.querySelector('.app'),
+  containerMovements: document.querySelector('.movements'),
 
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
+  btnLogin: document.querySelector('.login__btn'),
+  btnTransfer: document.querySelector('.form__btn--transfer'),
+  btnLoan: document.querySelector('.form__btn--loan'),
+  btnClose: document.querySelector('.form__btn--close'),
+  btnSort: document.querySelector('.btn--sort'),
 
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
+  inputLoginUsername: document.querySelector('.login__input--user'),
+  inputLoginPin: document.querySelector('.login__input--pin'),
+  inputTransferTo: document.querySelector('.form__input--to'),
+  inputTransferAmount: document.querySelector('.form__input--amount'),
+  inputLoanAmount: document.querySelector('.form__input--loan-amount'),
+  inputCloseUsername: document.querySelector('.form__input--user'),
+  inputClosePin: document.querySelector('.form__input--pin'),
+};
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
