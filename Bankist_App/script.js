335099d6e457cf66b55503758d3422aa2e50ad80
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-01 08:41:11 +0600

    all

--- Bankist_App/script.js
+++ Bankist_App/script.js
@@ -64,7 +64,7 @@ class Account {
   transferMoney(amount, transferTo) {
     const receiver = accounts.find(acc => acc.username === transferTo);
 
-    console.log(receiver, amount, transferTo);
+    if (receiver) console.log(receiver, amount, transferTo);
   }
 
   requestLoan(amount) {
