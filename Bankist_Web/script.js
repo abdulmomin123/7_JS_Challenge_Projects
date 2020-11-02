d654e036bc131be8bc213b87790cfdfdc9e7ab3d
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-03 03:56:05 +0600

    all

--- Bankist_Web/script.js
+++ Bankist_Web/script.js
@@ -6,26 +6,20 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+const elements = {
+  modalElements: document.querySelectorAll('.modal, .overlay'),
+  btnsOpenModal: document.querySelectorAll('.btn--show-modal'),
+  btnsCloseModal: document.querySelectorAll('.btn--close-modal, .overlay'),
 };
 
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+const openModal = e => {
+  e.preventDefault();
+  elements.modalElements.forEach(el => el.classList.remove('hidden'));
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+const closeModal = () => {
+  elements.modalElements.forEach(el => el.classList.add('hidden'));
+};
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
-});
-
-const elements = {};
+elements.btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+elements.btnsCloseModal.forEach(el => el.addEventListener('click', closeModal));
