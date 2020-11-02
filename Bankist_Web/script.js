c75b0baeb9c0d36a5da8213ff525b0cee9eee7b1
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-03 03:57:38 +0600

    all

--- Bankist_Web/script.js
+++ Bankist_Web/script.js
@@ -1,25 +1,23 @@
 'use strict';
 
-// Modal window
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
+// DOM Elements
 const elements = {
   modalElements: document.querySelectorAll('.modal, .overlay'),
   btnsOpenModal: document.querySelectorAll('.btn--show-modal'),
   btnsCloseModal: document.querySelectorAll('.btn--close-modal, .overlay'),
 };
 
+// Opens modal window
 const openModal = e => {
   e.preventDefault();
   elements.modalElements.forEach(el => el.classList.remove('hidden'));
 };
 
+// Closes modal window
 const closeModal = () => {
   elements.modalElements.forEach(el => el.classList.add('hidden'));
 };
 
+// Modal window opening & closing handlers
 elements.btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 elements.btnsCloseModal.forEach(el => el.addEventListener('click', closeModal));
