9ee2fc7b1752e373056a4bd82025e9c4cab712b3
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-03 04:15:20 +0600

    all

--- Bankist_Web/script.js
+++ Bankist_Web/script.js
@@ -15,8 +15,7 @@ const openModal = e => {
 
 // Closes modal window
 const closeModal = e => {
-  if (!e.key) elements.modalElements.forEach(el => el.classList.add('hidden'));
-  if (e.key === 'Escape')
+  if (!e.key || e.key === 'Escape')
     elements.modalElements.forEach(el => el.classList.add('hidden'));
 };
 
