0b6ec6452eea4a51b8f3178d9072d10aec1c8893
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-06 18:30:40 +0600

    all

--- Bankist_Web/script.js
+++ Bankist_Web/script.js
@@ -163,3 +163,5 @@ const imagesObserver = new IntersectionObserver(loadImage, {
   rootMargin: '150px',
 });
 elements.secImages.forEach(img => imagesObserver.observe(img));
+
+// Testimonials slider handler
